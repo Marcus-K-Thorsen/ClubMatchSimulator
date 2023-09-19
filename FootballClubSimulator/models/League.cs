d154950e53e04fa04e22693d27cdd98082cb0c36
Marcus <marc303i@stud.kea.dk>
2023-09-19 12:19:03 +0200

    Har indsat variabler for det nummer af positions af promotions til de forskellige ligaer, samt har lagt noter til det der mangler i League.cs. Mangler dog stadig at give promotion variabler en funktionalitet, men lige nu gemmer/writer og henter/reader de nye variabler, men som 0

--- FootballClubSimulator/models/League.cs
+++ FootballClubSimulator/models/League.cs
@@ -11,6 +11,12 @@ public class League
     
     // ___________________ IMPORTANT PROPERTIES/FIELDS _________________________________
     public string LeagueName { get; set; }
+    
+    public int ChampionsLeague { get; private set; }
+    public int EuropeLeague { get; private set; }
+    public int ConferenceLeague { get; private set; }
+    public int Promotion { get; private set; }
+    public int Relegation { get; private set; }
 
     private List<Club> _teams = new List<Club>();
     public List<Club> Teams
@@ -37,7 +43,6 @@ public class League
             });
         }
     }
-    
 
     private List<Round[]> _firstRounds = new List<Round[]>();
     
@@ -94,15 +99,20 @@ public class League
     public League(string[] leagueValues, List<Club> teams, List<Round[]> firstRounds, List<Round[]> secondRounds)
     {
         LeagueName = leagueValues[0];
+        ChampionsLeague = Convert.ToInt32(leagueValues[1]);
+        EuropeLeague = Convert.ToInt32(leagueValues[2]);
+        ConferenceLeague = Convert.ToInt32(leagueValues[3]);
+        Promotion = Convert.ToInt32(leagueValues[4]);
+        Relegation = Convert.ToInt32(leagueValues[5]);
         Teams = teams;
         FirstRounds = firstRounds;
         SecondRounds = secondRounds;
     }
     
-    public static string ConvertHeaderToCsvFormat = "LeagueName";
+    public static string ConvertHeaderToCsvFormat = "LeagueName,ChampionsLeague,EuropeLeague,ConferenceLeague,Promotion,Relegation";
     public string ConvertToCsvFormat()
     {
-        return $"{LeagueName}";
+        return $"{LeagueName},{ChampionsLeague},{EuropeLeague},{ConferenceLeague},{Promotion},{Relegation}";
     }
     
     // ___________________ SPECIAL METHODS _________________________________
@@ -115,5 +125,44 @@ public class League
         }
 
         return allRound;
+    }
+    
+    // TODO skal lave metode der kan tage de to lister af runder og give leaguens teams deres standing ud fra rundernes udkom, listen af teams gives position, sorteres og gives tilbage
+    public List<Club> CalculateTeamStandings()
+    {
+        // Reset alle leaguens standings
+        // Gå gennem alle de første runder og tilskriv standings til leaguens teams ud fra rundernes matchoutcome
+        // Opdel leaguens teams i upper og lower listerne ud fra deres points og derefter goal difference
+        // Gå gennem alle den anden mængde runder og tilskriv stnadings til leaguens teams ud fra rundernes matchoutcome
+        // sorter upper, lower og teams listerne ud fra deres standings og giv dem en position, som kan være delt
+        // return teams listen
+        return Teams;
+    }
+    
+    // TODO skal lave en metode der kan lave to nye lister af runder ud fra leaguens teams og kalde på standard metoden CalculateTeamStandings() og returne listen af teams
+    public List<Club> CalculateNewTeamStandings()
+    {
+        // Reset leaguens runde lister, første og anden
+        // For antalet af FirstAmountOfRounds skab en ny liste af nye runder ud fra leaguens teams
+        //      For hvert team i Teams skal det team gå igennem Teams og skippe sig selv og skabe en runde mellem den og et andet team
+        //          Skab en runde der lægges i en liste og tilskriv points og goal difference til de forskellige teams ud efter deres matchoutcome
+        //      Lav listen af runder til et array og læg det array i listen af første runder
+        // Split Teams i to, så dem med flest point ellers højst goal difference kommer i upper tier og resten i lower tier listen
+        // For antalet af SecondAmountOfRounds skab en ny liste af nye runder ud fra leaguens upper og lower teams
+        //      den første halvdel af SecondAmountOfRounds skab runder ud fra upper og den sidste halvdel skab runder ud fra lower
+        //      For hvert team i upper/lower teams skal det team gå igennem upper/lower teams og skipper sig selv og skabe en runde mellem den og et andet team
+        //          Skab en runde der lægges i en liste
+        //      Lav listen af runder til et array or læg det array i listen af anden runder
+        // Til sidst returnes metoden CalculateTeamStandings(), der giver listen af teams der er blevet udført runder på og sorteret ud efter rundernes matchoutcome
+        return CalculateTeamStandings();
+    }
+
+    // TODO få lavet metode i Club der kan resette den til på ny
+    private void ResetTeamsStandings()
+    {
+        foreach (Club team in Teams)
+        {
+            // team.ResetStandings(); // Kalder metode i Club instancen der sætter dens standings til at være helt ny.
+        }
     }
 }
